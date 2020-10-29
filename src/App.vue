91e56f37806843d2c3f29bf4e34a2cb53a2beb0f
Abdulmuktinur <abduldw9a@gmail.com>
2020-10-29 09:59:59 +0700

    update paling baru

--- src/App.vue
+++ src/App.vue
@@ -18,11 +18,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
 }
 
 img{
-  margin-top : 75px;
+  margin-top : 60px;
   width : 300px;
 }
 </style>
